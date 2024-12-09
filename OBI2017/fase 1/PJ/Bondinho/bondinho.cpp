69a1cfa271a20892f4c971fc5674826748f96dad
Kauan Henrique Franchini Ladeira <maribelladeira09@gmail.com>
2024-12-09 14:07:47 -0300

    Create bondinho.cpp

---
+++ OBI2017/fase 1/PJ/Bondinho/bondinho.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> //solução bondinho, obi 2017 fase 1 PJ, feito por Kauan Henrique Franchini Ladeira
+using namespace std;
+int main(){
+    int a,m;
+    cin>>a;
+    cin>>m;
+    if (a + m <= 50){cout<<'S'<<"\n";}
+    else{cout<<'N'<<"\n";}
+    return 0;
+}
