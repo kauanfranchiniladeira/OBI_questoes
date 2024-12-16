686bef84aa13f13ee35d1cb5b197c612d00c7e75
Kauan Henrique Franchini Ladeira <maribelladeira09@gmail.com>
2024-12-15 21:34:54 -0300

    Create basquete.cpp

---
+++ OBI2018/fase 1/PJ/Basquete_de_Robôs/basquete.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int d;
+  cin>>d;
+  if(d<=800){cout<<1<<"\n";}
+  else if(800 < d && d <= 1400){cout<<2<<"\n";}
+  else{cout<<3<<"\n";}
+  return 0;
+}
